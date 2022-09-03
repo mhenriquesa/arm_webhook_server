721f4a4ab0c90d549b38439d6ef897abcfd57d4d
M. Henrique S.A <henriqueator@gmail.com>
2022-09-03 00:12:05 -0300

    Aquirvos para deploy heroku adicionados

--- run.py
+++ run.py
@@ -2,4 +2,6 @@ from src import create_app
 
 app = create_app()
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True) Para rodar na maquina
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
