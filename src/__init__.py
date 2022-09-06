23ae9b2e288da244494be03a06699828708fbbe2
M. Henrique S.A <henriqueator@gmail.com>
2022-09-06 18:47:17 -0300

    Atualizado init com loggin

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from src.Config import Config
+import sys
+import logging
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.ERROR)
 
     from src.routes import main
     app.register_blueprint(main)
